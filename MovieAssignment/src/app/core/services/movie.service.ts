a575060dcb78c3962a1bd51d701406cab630d105
Gourav <gouravgirdhar94@gmail.com>
2019-06-21 13:03:50 +0530

    s

--- MovieAssignment/src/app/core/services/movie.service.ts
+++ MovieAssignment/src/app/core/services/movie.service.ts
@@ -8,13 +8,13 @@ import { Movie } from 'src/app/models/movie.model';
   providedIn: 'root'
 })
 export class MovieService {
-  apiKey = '';
+  apiKey = '2bc49ac29568477c0075c56cba2ed307';
   constructor(private httpClient: HttpClient) { }
 
   getTrendingMovies (): Observable<Movie[]> {
     return this.httpClient.get('https://api.themoviedb.org/3/trending/movie/week?api_key=' + this.apiKey)
       .pipe(map(data => {
-        return data.results;
+        return data['results'];
       }) );
     }
 
@@ -22,14 +22,14 @@ export class MovieService {
     return this.httpClient.get('https://api.themoviedb.org/3/movie/now_playing?api_key='
     + this.apiKey + '&language=en-US&include_adult=false')
     .pipe(map(data => {
-      return data.results;
+      return data['results'];
     }) );
   }
 
   getPopularMovies (): Observable<any> {
     return this.httpClient.get('https://api.themoviedb.org/3/movie/popular?api_key=' + this.apiKey + '&language=en-US&page=1')
     .pipe(map(data => {
-      return data.results;
+      return data['results'];
     }) );
   }
 }
