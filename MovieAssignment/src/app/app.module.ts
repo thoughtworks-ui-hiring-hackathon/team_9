b3db5d51269f73ae406cc8729416ee184241bae9
apple <apple@macs-MacBook-Pro.local>
2019-06-21 11:39:27 +0530

    components

--- MovieAssignment/src/app/app.module.ts
+++ MovieAssignment/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/shared components/header/header.component';
+import { FooterComponent } from './modules/shared components/footer/footer.component';
+import { HomePageComponent } from './modules/components/home-page/home-page.component';
+import { DetailsPageComponent } from './modules/components/details-page/details-page.component';
+import { MovieListComponent } from './modules/components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    DetailsPageComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule
